2a678ab3fd6e5539ae4c564e179fecff824e5c88
DRVicTor48 <66957584+DRVicTor48@users.noreply.github.com>
2021-04-01 17:02:31 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -4,7 +4,7 @@ def roll_dice():
     return roll
 print('Welcome to Dice Game')
 option=0
-while option != 4:
+while option != '4':
     option = input('Enter Your Choice \n 1. Roll Dice \n 2. Points Check \n 3. Select Dice \n 4.Exit')
     if option == '1':
         print("The dice gave :")
